62ba9a3e16027f9467aa7923b86aa2c602b17d39
ricky-rosay <111669764+ricky-rosay@users.noreply.github.com>
2022-10-01 09:35:20 -0400

    adding background functionality

--- script.js
+++ script.js
@@ -65,3 +65,21 @@ var interval = setInterval(function() {
     var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
     sevenPm.val(init7);
   }
+
+  function background () {
+      
+    $(".form-control").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        hour = parseInt(hour);
+        console.log(timeTest);
+        console.log(hour);
+  //      console.log(this);
+        if (hour > timeTest) {
+            $(this).addClass("past");
+        } else if (hour < timeTest) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+  }
